5d1afe39a7f46945e96e8b269dcf40a67d93c588
andreamcgrane <45667192+andreamcgrane@users.noreply.github.com>
2019-01-15 11:01:50 -0600

    Add files via upload

---
+++ HotSeatEhs-Trent/HomeTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  HomeTableViewCell.swift
+//  HotSeatEhs
+//
+//  Created by Trent on 1/5/19.
+//  Copyright © 2019 district196.org. All rights reserved.
+//
+
+import UIKit
+
+let cellIdentifier = "HomeTableViewCell"
+
+class HomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
